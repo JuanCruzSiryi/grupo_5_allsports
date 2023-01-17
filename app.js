4f1c077074640d16a46fe6d3f453aa099e166e90
Danilo Burbano <danilo.b.argoty@gmail.com>
2023-01-17 18:33:27 -0500

    Port was chaged

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express =  require('express');
 const path = require("path");
 const app = express();
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
 
 app.use(express.static("public"));
 
