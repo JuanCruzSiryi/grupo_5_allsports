b2f17875cb09b059ea0696ee73cbf4cc36e132fe
Danilo Burbano <danilo.b.argoty@gmail.com>
2023-02-17 21:51:44 -0500

    disable users icon

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const usersController = require('../controllers/usersController');
 
+/* CRUD */
+
+/* END CRUD */
+
 router.get('/register', usersController.register);
 
 router.get('/login', usersController.login);
