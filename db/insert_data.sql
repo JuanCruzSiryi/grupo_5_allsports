7a8e956af7574aabfa6cbc0dcba36e51b548aa81
Danilo Burbano <danilo.b.argoty@gmail.com>
2023-03-23 16:43:52 -0500

    insert data file was modified

--- db/insert_data.sql
+++ db/insert_data.sql
@@ -20,12 +20,28 @@ insert into brands (name)
 values ('Nike');
 
 /*Insertar categoria*/
-insert into category (name)
-values ('Masculino');
+insert into categories (name)
+values ('Hombre');
+insert into categories (name)
+values ('Mujer');
+insert into categories (name)
+values ('Infantil');
 
 /* Insertar producto */
-insert into products (name, description, price)
-values ('diseño ultra moderno y su suela air top es ideal para running', 36217);
+insert into products (name, description, image, price)
+values (
+    'Adidas Gran Prix', 
+    'diseño ultra moderno y su suela air top es ideal para running',
+    'zapa-adidas-azul.jpg',
+    36217
+  );
+insert into products (name, description, image, price)
+values (
+    'Adidas Galaxi 5', 
+    'diseño ultra moderno y su suela air top es ideal para running',
+    'zapa-adidas-blanca.jpg',
+    36217
+  );
 
 
 /*Insertar pais*/
@@ -41,5 +57,22 @@ insert into roles (name)
 values ('admin');
 
 /* Insertar nuevo usuario */
-insert into users (firstName, lastName, email, password, roleId)
-values ('Jhon', 'Doe', 'doe@gmail.com', '$2a$10$aXbI3VkW/Pfo8kFlmyH9aetdXJo7BrCeFF0YtwIAX3lb1twJbke9u', 2);
+insert into users (firstName, lastName, email, password, image, role_id)
+values (
+    'Jhon',
+    'Doe',
+    'doe@gmail.com',
+    'avatar-1677950424639',
+    '$2a$10$aXbI3VkW/Pfo8kFlmyH9aetdXJo7BrCeFF0YtwIAX3lb1twJbke9u',
+    2
+  );
+
+insert into users (firstName, lastName, email, password, image, role_id)
+values (
+    'Claudio',
+    'Martinez',
+    'martinezclaudio11@hotmail.com',
+    'avatar_03.png',
+    '$2a$10$aXbI3VkW/Pfo8kFlmyH9aetdXJo7BrCeFF0YtwIAX3lb1twJbke9u',
+    1
+  );
