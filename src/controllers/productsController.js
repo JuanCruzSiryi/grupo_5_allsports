6540c9603cb904944d7adef7ee0da1c794e55c69
Danilo Burbano <danilo.b.argoty@gmail.com>
2023-04-24 18:35:33 -0500

    Improving code: *Views, *Css, *Controllers

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -16,13 +16,7 @@ const productsController = {
   getProducts: () => {
     return JSON.parse(fs.readFileSync(productsPath, "utf-8"));
   },
-  // index: (req, res) => {
-  //   res.render("../views/products/index", {
-  //     title: "Products List",
-  //     stylesheetFile: "/products/index.css",
-  //     productsList: productsController.getProducts(),
-  //   });
-  // },
+
   index: async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     res.locals.page = page;
@@ -38,6 +32,9 @@ const productsController = {
     try {
       const products = await Product.findAll({
         include: ["category", "color", "size", "tag", "brand"],
+        order: [
+          ['createdAt', 'DESC'],
+        ],
         limit,
         offset
       });
@@ -99,17 +96,6 @@ const productsController = {
       res.send(error)
     }
   },
-  // show: (req, res) => {
-  //   let productId = req.params.id;
-  //   let product = productsController.getProducts().find((product) => product.id == productId);
-
-  //   res.render("products/show", {
-  //     title: "Mi Producto",
-  //     stylesheetFile: "/products/show.css",
-  //     product
-  //   });
-    
-  // },
 
 show: async (req, res) => {
     try {
@@ -135,7 +121,7 @@ show: async (req, res) => {
     const tags = await Tag.findAll();
     res.render('products/create', {
     title: "Crear producto",
-    stylesheetFile: "registerProduct.css",
+    stylesheetFile: "/products/create.css",
     colors,
     brands,
     sizes,
@@ -156,7 +142,7 @@ show: async (req, res) => {
           const tags = await Tag.findAll();
             return res.render('products/create', {
               title: 'Crear producto',
-              stylesheetFile: "registerProduct.css",
+              stylesheetFile: "/products/create.css",
               errors: errors.mapped(),
               oldBody: req.body,
               colors,
@@ -174,12 +160,15 @@ show: async (req, res) => {
       name: req.body.nameProduct || "sin nombre",
       description: req.body.descProduct || "sin descripcion",
       price: req.body.priceProduct || 0,
+      discount: req.body.discount || 0,
       image: image,
-      brand_id: req.body.brandProduct || "sin marca",
-      tag_id: req.body.tag || "sin tipo",
-      color_id: req.body.color || "sin color",
-      size_id: req.body.sizeProduct || "sin talle",
+      brand_id: req.body.brandProduct || 1,
+      tag_id: req.body.tag || 1,
+      color_id: req.body.color || 1,
+      size_id: req.body.sizeProduct || 1,
+      category_id: req.body.category || 1,
     };
+    console.log("Producto a crear: ", newProduct);
 
     Product.create(newProduct)
     .then(() => {
@@ -187,40 +176,7 @@ show: async (req, res) => {
     })
     .catch(error => res.send(error));
 
-    // CRUD NUEVO
-
-    // store: (req, res) {
-    //   const _body = {
-    //     name: req.body.nameProduct,
-    //     description: req.body.descProduct,
-    //     brand_id: req.body.brandProduct,
-    //     category_id: req.body.name.categoryProduct,
-    //     tag_id: req.body.name.tags,
-    //     color_id: req.body.name.color,
-    //     image: req.body.image,
-    //     price: req.body.priceProduct,
-    //     size_id: req.body.talles,
-    //     discount: req.body.discount,
-    //     state: req.body.state
-    //   }
-    //   db.Product.create(_body)
-    //    .then(() => {
-    //        res.redirect('/products')
-    //    })
-    //    .catch(error => res.send('error'));
-    // }
-
   },
-  // edit: (req, res) => {
-  //   let productId = req.params.id;
-  //   let product = productsController.getProducts().find((product) => product.id == productId);
-
-  //   res.render("../views/products/productEdit", {
-  //     title: "Mi product",
-  //     stylesheetFile: "editProduct.css",
-  //     product
-  //   });
-  // },
 
   edit: async (req, res) => {
     try {
@@ -233,7 +189,7 @@ show: async (req, res) => {
           
       res.render("../views/products/edit", {
         title: "Mi Product",
-        stylesheetFile: "editProduct.css",
+        stylesheetFile: "products/edit.css",
         product,
         colors,
         brands,
@@ -247,43 +203,8 @@ show: async (req, res) => {
     }
   },
 
-  
-  // update: (req, res) => {
-  //   let productId = req.params.id;
-  //   // console.log("body: ", req.body);
-  //   let products = productsController.getProducts();
-
-  //   products.forEach((product, index) => {
-  //     if (product.id == productId) {
-  //       product.name = req.body.nameProduct || product.name;
-  //       product.description = req.body.descProduct || product.description;
-  //       product.price = req.body.priceProduct || product.price;
-  //       product.image = req.file? req.file.filename : product.image;
-  //       product.category = req.body.categoryProduct || product.category;
-  //       product.brand = req.body.brand || product.brand;
-  //       product.color = req.body.color || product.color;
-  //       product.size = req.body.talles || product.size;
-  //       product.available = true;
-
-  //       products[index] = product;
-  //     }
-  //   });
-
-  //   fs.writeFileSync(productsPath, JSON.stringify(products, null, "  "));
-
-  //   res.redirect("/products");
-  // },
-
   update: async (req, res) => { 
     const product = await Product.findByPk(req.params.id);
-    const colors = await Color.findAll();
-    const brands = await Brand.findAll();
-    const sizes = await Size.findAll();
-    const categories = await Category.findAll();
-    const tags = await Tag.findAll();
-
-
-    console.log(req.body);
 
     Product.update(
       { name: req.body.name,
@@ -308,39 +229,18 @@ show: async (req, res) => {
       });
   },
 
-  // delete: (req, res) => {
-  //   let productId = req.params.id;
-  //   let product = productsController.getProducts().find((product) => product.id == productId);
-  //   res.render('products/delete', {
-  //     title: "Borrar producto",
-  //     stylesheetFile: "editProduct.css",
-  //     product
-  //   });
-  // },
-
   delete: async (req, res) => {
     try {
       const product = await Product.findByPk(req.params.id);
       res.render("products/delete", {
         title: "Borrar producto",
-        stylesheetFile: "/editProduct.css",
+        stylesheetFile: "/products/edit.css",
         product
       });
     } catch (error) {
       res.send(error)
     }
   },
-  
-
-  // destroy: (req, res) => {
-  //   let productId = req.params.id;
-  //   let products = productsController.getProducts();
-  //   let newProducts = products.filter(product => product.id != productId)
-
-  //   fs.writeFileSync(productsPath, JSON.stringify(newProducts, null, "  "));
-
-  //   res.redirect("/products");
-  // },
 
   destroy: (req, res) => {
     Product.destroy({
@@ -376,19 +276,19 @@ show: async (req, res) => {
   productCart: (req, res) => {
     res.render("../views/products/productCart", {
       title: "Product-Cart",
-      stylesheetFile: "productCart.css",
+      stylesheetFile: "/products/cart.css",
     });
   },
   productRegister: (req, res) => {
     res.render("../views/products/productRegister", {
       title: "Products-Register",
-      stylesheetFile: "registerProduct.css",
+      stylesheetFile: "/products/create.css",
     });
   },
   productEdit: (req, res) => {
     res.render("../views/products/productEdit", {
       title: "Products-Edit",
-      stylesheetFile: "editProduct.css",
+      stylesheetFile: "products/edit.css",
     });
   },
 };
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -7,20 +7,13 @@ const { validationResult } = require('express-validator');
 const db = require('../database/models');
 const Op = db.Sequelize.Op;
 
-const {User} = require('../database/models');
+const {User, Country} = require('../database/models');
 
 const usersController = {
   /* CRUD */
   getUsers: () => {
     return JSON.parse(fs.readFileSync(usersPath, "utf-8"));
   },
-  // index: (req, res) => {
-  //   res.render("../views/users/index", {
-  //     title: "Lista de usuarios",
-  //     stylesheetFile: "/users/index.css",
-  //     usersList: usersController.getUsers(),
-  //   });
-  // },
   index: async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     res.locals.page = page;
@@ -101,17 +94,6 @@ const usersController = {
       res.send(error)
     }
   },
-  // SHOW
-  // show: (req, res) => {
-  //   let profileId = req.params.id;
-  //   let profile = usersController.getUsers().find((profile) => profile.id == profileId);
-  
-  //   res.render("users/show", {
-  //     title: "Profile",
-  //     stylesheetFile: "users/show.css",
-  //     user: profile
-  //   });
-  // },
   show: async (req, res) => {
     try {
       const user = await User.findByPk(req.params.id,
@@ -132,77 +114,20 @@ const usersController = {
     // req.session.userLogged = user;
     res.render("users/profile", {
       title: "Profile",
-      stylesheetFile: "profile.css",
+      stylesheetFile: "users/profile.css",
       user: profile
     });
   },
 
   // CREATE
-  register: (req, res) => {
+  register: async (req, res) => {
+    const countries = await Country.findAll();
     res.render("users/register", {
       title: "Register",
-      stylesheetFile: "register.css",
+      stylesheetFile: "users/register.css",
+      countries
     });
   },
-
-  // STORE
-  // store: (req, res) => {
-  //       const errors = validationResult(req);
-        
-  //       if ( ! errors.isEmpty() ) {
-  //           return res.render('users/register', {
-  //             title: 'Nuevo usuario',
-  //             stylesheetFile: "register.css",
-  //             errors: errors.mapped(),
-  //             oldBody: req.body,
-  //       })
-  //       }
-        
-  //       let users = usersController.getUsers();
-  //       // let images = [];
-        
-  //       // if (req.files) {
-  //       //     req.files.forEach(file => {
-  //       //         images.push({
-  //       //             "id": Date.now(),
-  //       //             "name": file.filename,
-  //       //         });
-  //       //     });
-  //       // } else {
-  //       //     images.push("default-user.png");
-  //       // }
-  //       let image = req.file? req.file.filename : "default-user.png";
-        
-  //       let newUser = {
-  //           "id": uuidv4(),
-  //           "firstName": req.body.firstName || "Sin nombre",
-  //           "lastName": req.body.lastName || "Sin apellido",
-  //           "email": req.body.email || "Sin email",
-  //           "password": bcryptjs.hashSync(req.body.password, 10) || "Sin contraseña",
-  //           "category": "Usuario",
-  //           "image": image,
-  //           "available": true
-  //       }
-        
-  //       users.push(newUser);
-        
-  //       fs.writeFileSync(usersPath, JSON.stringify(users, null, '  '));
-  //       // Login the user registered
-  //       if(!res.locals.userLogged) {
-  //         req.session.userLogged = newUser;
-  //         res.locals.userLogged = newUser;
-  //         res.cookie(
-  //           'userLogged',
-  //           newUser,
-  //           { maxAge: 1000 * 60 * 5 }
-  //         );
-  //         // console.log("# Register and Login");
-  //         return res.redirect('/');
-  //       }
-  //       res.redirect('/users');
-  // },
-
-   // CRUD NUEVO
 
   store: (req, res) =>{
       const errors = validationResult(req);
@@ -210,7 +135,7 @@ const usersController = {
         if ( ! errors.isEmpty() ) {
             return res.render('users/register', {
               title: 'Nuevo usuario',
-              stylesheetFile: "register.css",
+              stylesheetFile: "users/register.css",
               errors: errors.mapped(),
               oldBody: req.body,
         })
@@ -221,70 +146,33 @@ const usersController = {
         firstName: req.body.firstName || "Sin nombre",
         lastName: req.body.lastName || "Sin apellido",
         email: req.body.email || "Sin email",
-        password: bcryptjs.hashSync(req.body.password, 10) || "Sin contraseña",
+        password: bcryptjs.hashSync(req.body.password, 10),
         image: image,
-        role_id: 1,
         address: req.body.address || "Sin dirección",
-        country_id: 2,
+        country_id: req.body.country,
+        role_id: 1,
         state: 1,
       }
        User.create(newUser)
        .then(() => {
-           res.redirect('/users')
+          res.redirect('/users')
        })
        .catch(error => res.send(error));
   },
-
-  // EDIT
-  // edit: (req, res) => {
-  //   let userId = req.params.id;
-  //   let user = usersController.getUsers().find((user) => user.id == userId);
-  //   res.render("../views/users/edit", {
-  //     title: "Mi User",
-  //     stylesheetFile: "editUser.css",
-  //     user
-  //   });
-  // },
   edit: async (req, res) => {
     try {
       const user = await User.findByPk(req.params.id);
+      const countries = await Country.findAll();
       res.render("../views/users/edit", {
         title: "Mi User",
-        stylesheetFile: "editUser.css",
-        user
+        stylesheetFile: "/users/edit.css",
+        user,
+        countries
       });
     } catch (error) {
       res.send(error)
     }
   },
-
-  // UPDATE
-  // update: (req, res) => {
-  //   let userId = req.params.id;
-  //   // console.log("body: ", req.body);
-  //   let users = usersController.getUsers();
-
-  //   users.forEach((user, index) => {
-  //     if (user.id == userId) {
-  //       // user.firstName = req.body.firstName || user.first_name;
-  //       user.firstName = req.body.firstName || user.firstName;
-
-  //       // user.last_name = req.body.last_nameUser || user.last_name;
-  //      user.lastName = req.body.lastName || user.lastName;
-  //       // user.email = req.body.emailUser || user.email;
-  //       user.email = req.body.email || user.email;
-  //       user.image = req.file? req.file.filename : user.image;
-  //       // user.image = req.file? req.file.image : user.image;
-  //       // user.paswword = req.body.paswwordUser || user.paswword;
-  //       // user.password = bcryptjs.hashSync(req.body.password, 10);
-  //       user.category = req.body.categoryUser || user.category;
-  //       user.available = true;
-  //       users[index] = user;
-  //     }
-  //   });
-  //   fs.writeFileSync(usersPath, JSON.stringify(users, null, "  "));
-  //   res.redirect("/users");
-  // },
 
   update: async (req, res) => {
     const user = await User.findByPk(req.params.id);
@@ -295,6 +183,7 @@ const usersController = {
         email: req.body.email,
         image: req.file? req.file.filename : user.image,
         category: req.body.categoryUser,
+        country_id: req.body.country,
       },
       {
         where: {id: req.params.id}
@@ -307,22 +196,12 @@ const usersController = {
       });
   },
 
-  // DELETE
-  // delete: (req, res) => {
-  //   let userId = req.params.id;
-  //   let user = usersController.getUsers().find((user) => user.id == userId);
-  //   res.render('users/delete', {
-  //     title: "Borrar usuario",
-  //     stylesheetFile: "/users/editUser.css",
-  //     user
-  //   });
-  // },
   delete: async (req, res) => {
     try {
       const user = await User.findByPk(req.params.id);
       res.render("users/delete", {
         title: "Borrar usuario",
-        stylesheetFile: "/users/editUser.css",
+        stylesheetFile: "/users/delete.css",
         user: user
       });
     } catch (error) {
@@ -330,15 +209,6 @@ const usersController = {
     }
   },
   
-  // DESTROY
-  // destroy: (req, res) => {
-  //   let userId = req.params.id;
-  //   let users = usersController.getUsers();
-  //   let newUsers = users.filter(user => user.id != userId)
-  //   fs.writeFileSync(usersPath, JSON.stringify(newUsers, null, "  "));
-  //   res.redirect("/users");
-  // },
-
   destroy: (req, res) => {
     User.destroy({
       where: {id: req.params.id}
@@ -350,7 +220,7 @@ const usersController = {
         res.send(error);
       });
   },
-  /* END CRUD */
+
 };
 
 module.exports = usersController;
