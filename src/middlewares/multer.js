4a3120faa6b94e812f80a6634996a39c19d50aec
Maxi-Sio <msio94@hotmail.com>
2023-04-29 16:35:06 -0300

    validation of image

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,17 +1,27 @@
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage( {
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const type = req.originalUrl.includes("products")? "products" : "users";
+    const type = req.originalUrl.includes("products") ? "products" : "users";
     cb(null, path.join(__dirname, `../../public/images/${type}`))
   },
   filename: (req, file, cb) => {
-    const name = req.originalUrl.includes("products")? "product-" : "avatar-";
+    const name = req.originalUrl.includes("products") ? "product-" : "avatar-";
     cb(null, name + Date.now() + path.extname(file.originalname))
   }
 })
 
-const upload = multer({storage: storage})
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('El formato de la imagen debe ser jpeg, jpg, png o gif'));
+    }
+  }
+});
 
 module.exports = upload;
