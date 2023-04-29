5c223240a24fad653ba44a007f89f3d870818c8d
Maxi-Sio <msio94@hotmail.com>
2023-04-29 16:11:22 -0300

    validations in back

--- src/middlewares/productValidator.js
+++ src/middlewares/productValidator.js
@@ -2,9 +2,9 @@ const { body } = require('express-validator');
 
 const rules = [
     body('nameProduct')
-        .isLength({ min: 1 }).withMessage('El campo no debe estar vacío.'),
+        .isLength({ min: 5 }).withMessage('El nombre del producto es obligatorio y debe tener como mínimo 5 caracteres.'),
     body('descProduct')
-        .isLength({ min: 1 }).withMessage('El campo no debe estar vacío.'), 
+        .isLength({ min: 20 }).withMessage('La descripción del producto es obligatoria y debe tener como mínimo 20 caracteres.'), 
 ];
 
 module.exports = rules;
