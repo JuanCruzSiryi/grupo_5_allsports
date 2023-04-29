b2c5b4f96b00e2589a1d3d611edcc91af98a3a4f
LeandroLopezsv <leandrolopezsv@gmail.com>
2023-04-29 12:56:49 -0300

    Validation Edit and Create Product is ok, only review Images

--- public/js/products/create.js
+++ public/js/products/create.js
@@ -3,35 +3,76 @@ const form = document.getElementById('formCreateProduct')
 document.addEventListener('DOMContentLoaded', formulary)
 
 function formulary (){
-form.nameProduct.addEventListener ('blur', validationName);
-form.descProduct.addEventListener ('blur', validationDesc)
-
+   form.nameProduct.addEventListener ('blur', validationName);
+   form.descProduct.addEventListener ('blur', validationDesc)
+   form.discount.addEventListener ('blur', validationDisc)
+   form.priceProduct.addEventListener ('blur', validationPrice)
+   form.image.addEventListener ('blur', validationImage)
 }
 
 function validationName (e){
- // let input = e.target
- // if (input.tagName == "INPUT"){ //  
 e.preventDefault()
-
+let errorName = document.querySelector('.nameProductError')
+   
 if (form.nameProduct.value.length < 5){
- // document.getElementById(p).innerHTML = "El nombre es obligatorio"
-
-//  alert ("El nombre es obligatorio")
- nameProduct.innerHTML = '<p> El nombre es obligatorio </p>' 
+   errorName.innerHTML = 'El nombre debe contener al menos 5 caracteres'
+ } else{
+   errorName.innerHTML = ''
  }
  }
 
-function validationDesc (e){
- //  
- e.preventDefault()
+ function validationDesc (e){
+    e.preventDefault()
+    let errorDesc = document.querySelector('.descProductError')
+   
+ if (form.descProduct.value.length < 20){
 
-if (form.descProduct.value.length < 20){
-
- alert ("La descripción debe contener al menos 20 caracteres")
-
-            // let errorDesc = document.getElementById("error")
- // errorDesc.innerHTML = '<p class= "text-error"> El mail es obligatorio </p>'
+      errorDesc.innerHTML = 'La descripción debe contener al menos 20 caracteres'
+ } else{
+      errorDesc.innerHTML = ''
  }
-}
+ }
+
+ function validationDisc (e){  
+  e.preventDefault()
+  let errorDisc = document.querySelector('.discountError')
+ 
+  if (form.discount.value > 99){
+ 
+   errorDisc.innerHTML ="El descuento no puede ser mayor a 99%"
+ 
+}else{
+        errorDisc.innerHTML = ''
+  }
+ }
+ 
+ function validationPrice (e){ 
+    e.preventDefault()
+    let errorPrice = document.querySelector('.priceError')
+  
+   if (form.price.value == 0){
+   
+      errorPrice.innerHTML = 'El precio no puede ser $0'
+               // let errorDesc = document.getElementById("error")
+    // errorDesc.innerHTML = '<p class= "text-error"> El mail es obligatorio </p>'
+    }else{
+      errorPrice.innerHTML = ''
+    }
+   }
+   
+// FALTA CORREGIR!!!!!!!!!
+
+   function validationImage (e){ 
+      e.preventDefault()
+      let errorImage = document.querySelector('.imageError')
+    
+     if (!form.image.value == "JPG" || "JPEG" ||"PNG" || "GIT"){
+     
+      errorImage.innerHTML ="El archivo solo puede ser JPG, JPEG, PNG o GIF"
+ 
+     }else{
+         errorImage.innerHTML = ''            
+      }
+     }
 
 
--- public/js/products/edit.js
+++ public/js/products/edit.js
@@ -5,34 +5,82 @@ document.addEventListener('DOMContentLoaded', formulary)
 function formulary (){
     form.name.addEventListener ('blur', validationName);
     form.description.addEventListener ('blur', validationDesc)
-    let firstNameError = this.document.querySelector('.firstNameError')
-    let lastNameError = this.document.querySelector('.lastNameError')
-
+    form.discount.addEventListener ('blur', validationDisc)
+    form.price.addEventListener ('blur', validationPrice)
+    form.image.addEventListener ('blur', validationImage)
 
 }
 
 function validationName (e){
- // let input = e.target
- // if (input.tagName == "INPUT"){ //  
+
 e.preventDefault()
-
+let errorName = document.querySelector('.nameProductError')
+    
 if (form.name.value.length < 5){
- // document.getElementById(p).innerHTML = "El nombre es obligatorio"
-
- alert ("El nombre es obligatorio")
-//  firstNameError.innerHTML = '<p> El nombre es obligatorio </p>' 
+    errorName.innerHTML = 'El nombre debe contener al menos 5 caracteres'
+     } else{
+       errorName.innerHTML = ''
+     }
  }
- }
+    
 
 function validationDesc (e){
- //  
  e.preventDefault()
+ let errorDesc = document.querySelector('.descProductError')
+
 
 if (form.description.value.length < 20){
 
- alert ("La descripción debe contener al menos 20 caracteres")
-
-            // let errorDesc = document.getElementById("error")
- // errorDesc.innerHTML = '<p class= "text-error"> El mail es obligatorio </p>'
- }
+    errorDesc.innerHTML = 'La descripción debe contener al menos 20 caracteres'
+} else{
+     errorDesc.innerHTML = ''
 }
+}
+
+
+function validationDisc (e){ 
+    e.preventDefault()
+    let errorDisc = document.querySelector('.discountError')
+   
+ 
+    if (form.discount.value > 99){
+   
+        errorDisc.innerHTML ="El descuento no puede ser mayor a 99%"
+   
+    }else{
+            errorDisc.innerHTML = ''
+      }
+     }
+   
+
+   function validationPrice (e){ 
+    e.preventDefault()
+    let errorPrice = document.querySelector('.priceError')
+   
+   if (form.price.value == 0){
+   
+        errorPrice.innerHTML = 'El precio no puede ser $0'
+ 
+    }else{
+        errorPrice.innerHTML = ''
+    }
+    }
+   
+   
+   function validationImage (e){ 
+
+   // FALTA CORREGIR!!!!!!!!!
+
+   function validationImage (e){ 
+    e.preventDefault()
+    let errorImage = document.querySelector('.imageError')
+  
+   if (!form.image.value == "JPG" || "JPEG" ||"PNG" || "GIT"){
+   
+    errorImage.innerHTML ="El archivo solo puede ser JPG, JPEG, PNG o GIF"
+
+   }else{
+       errorImage.innerHTML = ''            
+    }
+   }
+   }
