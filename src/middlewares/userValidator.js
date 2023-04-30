5a57cc17f8826474b3fce6061fcab44d565952d2
JuanCruzSiryi <juancruzsiryi@gmail.com>
2023-04-29 21:40:27 -0300

    create and login users

--- src/middlewares/userValidator.js
+++ src/middlewares/userValidator.js
@@ -7,6 +7,7 @@ const rules = [
         .isLength({ min: 1 }).withMessage('El campo no debe estar vacío.'),
     body('email')
         .isLength({ min: 2 }).withMessage('Por lo menos necesito 2 caracteres'),
+        // .isEmail('asd@gmail.com').withMessage('No es un formato de email válido.')
     body('password')
         .isLength({ min: 8 }).withMessage('Por lo menos necesito 8 caracteres')
                  
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,5 +1,6 @@
 const express =  require('express');
 const router = express.Router();
+const rules = require('../middlewares/userValidator');
 
 const mainController = require('../controllers/mainController');
 const guestMiddleware = require('../middlewares/guestMiddleware');
@@ -8,7 +9,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.get('/', mainController.index);
 
 router.get('/login', authMiddleware, mainController.login);
-router.post('/login', authMiddleware, mainController.processLogin);
+router.post('/login', authMiddleware, rules, mainController.processLogin);
 
 router.get('/logout', mainController.logout);
 
