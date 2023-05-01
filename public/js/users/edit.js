0b21d961915cd38b579cc997ac7b4b5ffafe7461
Danilo Burbano <danilo.b.argoty@gmail.com>
2023-04-30 21:24:03 -0500

    validation in edit user on the front side was added

--- public/js/users/edit.js
+++ public/js/users/edit.js
@@ -5,8 +5,6 @@ window.addEventListener('DOMContentLoaded', function() {
   let firstNameError = this.document.querySelector('.firstNameError')
   let lastNameError = this.document.querySelector('.lastNameError')
   let emailError = this.document.querySelector('.emailError')
-  let passwordError = this.document.querySelector('.passwordError')
-  let termsError = this.document.querySelector('.termsError')
 
   console.log("Form: ", form);
 
@@ -14,14 +12,12 @@ window.addEventListener('DOMContentLoaded', function() {
   form.addEventListener('submit', register);
 
   function register(evt) {
-    console.log("Botooooon");
+    console.log("Botooooon ## 2");
     // console.log("el elemento", evt.target);
     let errores = {}
     if(form.firstName.value.length < 2) errores.firstName = 'El Nombre debe tener más de 2 caracteres'
     if(form.lastName.value.length < 2) errores.lastName = 'El Apellio debe tener más de 2 caracteres'
-    if(form.email.value.length < 2) errores.emai = 'El Email debe ser válido'
-    if(form.password.value.length < 8) errores.password = 'La contraseña debe tener al menos 8 caracteres'
-    if(!form.terms.checked) errores.terms = 'Debes aceptar los terminos'
+    if(form.email.value.length < 2) errores.email = 'El Email debe ser válido'
     
     // console.log("is email?: ", validator.isEmail(form.email.value));
 
@@ -32,8 +28,6 @@ window.addEventListener('DOMContentLoaded', function() {
       firstNameError.innerHTML = (errores.firstName) ? `<li> ${errores.firstName} </li>` : '';
       lastNameError.innerHTML = (errores.lastName) ? `<li> ${errores.lastName} </li>` : '';
       emailError.innerHTML = (errores.email) ? `<li> ${errores.email} </li>` : '';
-      passwordError.innerHTML = (errores.password) ? `<li> ${errores.password} </li>` : '';
-      termsError.innerHTML = (errores.terms) ? `<li> ${errores.terms} </li>` : '';
     } else {
       form.submit();
     }
